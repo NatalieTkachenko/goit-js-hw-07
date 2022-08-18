684168799d232c8f0e8520a66d44174da28306d5
Natalie Tkachenko <tkachenko_nataliya83@ukr.net>
2022-08-18 15:58:49 +0300

    1st try for hw 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,80 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryArea = document.querySelector( ".gallery" );
+
+const galleryCollection = galleryItems.map( ( { preview, original, description } ) =>
+{
+    const galleryImage = document.createElement( 'img' );
+    galleryImage.classList.add( 'gallery__image' );
+    galleryImage.setAttribute( 'src', `${ preview }` );
+    galleryImage.setAttribute( 'data-source', `${ original }` ); ``
+    galleryImage.setAttribute( 'alt', `${ description }` );
+    
+
+    const galleryLink = document.createElement( 'a' );
+    galleryLink.classList.add( 'gallery__link' );
+    galleryLink.setAttribute( 'href', `${ original }` );
+    galleryLink.append( galleryImage );
+
+
+    const galleryItem = document.createElement( 'div' );
+    galleryItem.classList.add( 'gallery__item' );
+    galleryItem.append( galleryLink );
+    
+    return galleryItem;
+       
+} );
+
+
+galleryArea.append( ...galleryCollection );
+
+
+galleryArea.addEventListener( 'click', showBigImageHandler );
+
+
+
+function showBigImageHandler( evt ) 
+{
+    event.preventDefault();
+    if ( event.target.nodeName !== 'IMG' )
+    {
+        return;
+    }
+    else
+    {
+        const bigImageLink = event.target.dataset.source;
+        const modal = basicLightbox.create( `<img src="${ bigImageLink }" width="800" height="600">` );
+
+        modal.show();
+        
+    } 
+    
+};
+
+
+
+document.addEventListener( 'keydown', closeModalWithEsc );
+
+function closeModalWithEsc( evt )
+{
+    const modalIsOpen = document.querySelector( '.basicLightbox' );
+    const escIsPressed = evt.code === 'Escape';
+
+    if ( !modalIsOpen || !escIsPressed )
+    {
+        return;
+    }
+    else
+    {
+        modalIsOpen.classList.remove( 'basicLightbox' );
+    }
+
+}
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector( '.gallery' );
+console.log( gallery );
+
+const collection = galleryItems.map( ( { preview, original, description } ) =>
+{
+  const galleryImage = document.createElement( 'img' );
+  galleryImage.classList.add( 'gallery__image' );
+  galleryImage.setAttribute( 'src', `${preview}` );
+  galleryImage.setAttribute( 'alt', `${description}` );
+
+  const galleryItem = document.createElement( 'a' );
+  galleryItem.classList.add( 'gallery__item' );
+  galleryItem.setAttribute( 'href', `${original}` );
+  galleryItem.appendChild( galleryImage );
+
+  return galleryItem;
+
+} );
+
+gallery.append( ...collection );
+
+
+
+console.log( galleryItems );
+
+gallery.addEventListener( 'click', openModalHandler );
+
+function openModalHandler( evt )
+{
+  event.preventDefault();
+  if ( event.target.nodeName !== 'IMG' )
+  {
+    return;
+  }
+  
+    
+  
+  const lightbox = new SimpleLightbox( '.gallery__item',
+    {
+      captions: true,
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+    });
+  
+  return lightbox;
+};
